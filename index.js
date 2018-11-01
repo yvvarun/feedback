2c89994fb9fd6dd62906dcc9e8b10712cf3a4f27
yvvarun <yvvarun@gmail.com>
2018-11-01 22:33:15 +0530

    oauth2.0 phase 1

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const passport = require("passport");
+//TODO: not sure if .Strategy is needed. check.
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-app.get("/", (req, res) => {
-  res.send({ hey: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
